7ef6850092aa82f4c830497bd227868909a965a0
Lipraxde <B10430214@mail.ntust.edu.tw>
2018-05-22 20:35:56 +0800

    Eliminate error tag line leader whitespace.

--- project4_1/err.c
+++ project4_1/err.c
@@ -5,8 +5,6 @@
 #include "err.h"
 #include "main.h"
 
-#define outerr stdout
-
 #define eprintf(loc, msg)   printf("<Error> found in Line %d: ", loc.first_line);   \
                             printf("%s\n", msg);
 
@@ -14,16 +12,30 @@ void print_tagline(const YYLTYPE *loc)
 {
     int i;
     int c;
-    fprintf(outerr, "    %s", prog_line[loc->first_line-1]);
+    char *tag_line = strdup(prog_line[loc->first_line-1]);
+    int elim = 0;
+    int begin_col;
+    int end_col;
+    for(; (tag_line[0]!=0)&&
+        ((tag_line[0]==' ')||(tag_line[0]=='\t'));
+        elim++)
+        tag_line = &tag_line[1];
+
+    begin_col = loc->first_column - elim;
+    end_col = loc->last_column - elim;
+
+    fprintf(outerr, "    %s", tag_line);
     fprintf(outerr, "    ");
-    for(i=0; i<loc->first_column; i++)
+    for(i=0; i<begin_col; i++)
         fprintf(outerr, " ");
     if(loc->first_line == loc->last_line)
-        c = loc->last_column;
+        c = end_col;
     else
-        c = strlen(prog_line[loc->first_line-1]);
-    for(; (i<c)&&(i<loc->first_column+32); i++)
+        c = strlen(tag_line);
+    for(; (i<c)&&(i<begin_col+31); i++)
         fprintf(outerr, "^");
+    if(i == begin_col+32)
+        fprintf(outerr, "~");
     
     fprintf(outerr, "\n");
 }
@@ -72,6 +84,22 @@ int check_progname(const char *file_name, struct program_node *ast)
         print_tagline(&ast->loc);
         fprintf(outerr, "Ending at line %d:\n", ast->end_loc.first_line);
         print_tagline(&ast->end_loc);
+        fprintf(outerr, "\033[0m");
+        return 1;
+    }
+
+    return 0;
+}
+
+
+int check_rettype(struct type_node *ret_type, struct type_node *expr_type, const YYLTYPE *loc)
+{
+    if(ret_type->type == VOID_TYPE)
+    {
+        fprintf(outerr, "\033[31m");
+        fprintf(outerr, "<Error> current return type is void\n");
+        fprintf(outerr, "Should not return value at:\n");
+        print_tagline(loc);
         fprintf(outerr, "\033[0m");
         return 1;
     }
